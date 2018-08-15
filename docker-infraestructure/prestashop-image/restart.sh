d2a14e9e7c2e21f207348ac770457e6953011bc2
Federico Baroni <fbaroni@gmail.com>
2018-08-15 09:27:24 -0300

    se trata de hacer correr imagen prestashop

--- docker-infraestructure/prestashop-image/restart.sh
+++ docker-infraestructure/prestashop-image/restart.sh
@@ -7,19 +7,23 @@ password_db=root
 forwarded_port=8084
 forwarded_db_port=3038
 image_name=prestashop-conaiisi
+network_prestashop=network_prestashop
+network_balancer=network_prestashop
 
 docker stop ${container_db_name} || true
 docker rm ${container_db_name} || true
 
-docker run -d -p ${forwarded_db_port}:3036 -v ${PWD}/mysql:/var/lib/mysql \
+docker run -p ${forwarded_db_port}:3036 -v ${PWD}/mysql:/var/lib/mysql \
            --hostname=${container_db_name} \
            --name=${container_db_name} \
            -e MYSQL_ROOT_PASSWORD=${password_db}  \
            -d mysql:latest
 
-#docker network connect lamp_network ${container_name}
-sudo chmod 777 -R mysql/
 
+#docker network create ${network_prestashop} 
+docker network connect ${network_prestashop} ${container_db_name}
+sudo chmod 777 -R mysql/
+sudo chmod 777 -R www/
 #docker run -ti --name some-prestashop --link some-mysql -e DB_SERVER=some-mysql -p 8080:80 -d prestashop/prestashop
 
 docker stop ${container_prestashop_name} || true
@@ -40,16 +44,19 @@ docker build \
         --build-arg no_proxy=${_no_proxy} .
 
 
-docker run -d --hostname=${container_prestashop_name} \
+docker run --hostname=${container_prestashop_name} \
            --link ${container_db_name} \
            --name=${container_prestashop_name} \
+           --network=${network_prestashop} \
+           -e DB_HOSTNAME=${container_db_name} \
            -v ${PWD}/www:/var/www/html \
            -e PS_INSTALL_AUTO=1 \
            -e PS_ERASE_DB=1 \
            -p ${forwarded_port}:80 \
-           -d prestashop/prestashop
+           -d ${image_name}
 
+echo "conectar a la red---";
+docker network connect ${network_prestashop} ${container_prestashop_name}
 
-docker exec ${container_prestashop_name} chmod 777 -R /var/www/html/
-docker exec ${container_prestashop_name} service apache2 restart
+echo "BASH";
 docker exec -it ${container_prestashop_name} /bin/bash
