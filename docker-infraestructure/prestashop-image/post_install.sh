fd7ed92a1db34f42099c999cab24230a2826e5ff
Federico Baroni <fbaroni@gmail.com>
2018-08-15 07:21:30 -0300

    docker file

---
+++ docker-infraestructure/prestashop-image/post_install.sh
@@ -0,0 +1,3 @@
+chmod 777 -R /var/www/html/
+chown root:www-data -R /var/www/html/
+service apache2 restart
--- docker-infraestructure/prestashop-image/restart.sh
+++ docker-infraestructure/prestashop-image/restart.sh
@@ -6,6 +6,7 @@ container_prestashop_name=my-prestashop
 password_db=root
 forwarded_port=8084
 forwarded_db_port=3038
+image_name=prestashop-conaiisi
 
 docker stop ${container_db_name} || true
 docker rm ${container_db_name} || true
@@ -24,12 +25,31 @@ sudo chmod 777 -R mysql/
 docker stop ${container_prestashop_name} || true
 docker rm ${container_prestashop_name} || true
 
+proxy=${http_proxy}
+_no_proxy=${no_proxy}
+
+#BUILD DOCKER IMAGE
+docker build \
+        -t  ${image_name} \
+        --build-arg HTTP_PROXY=${proxy} \
+        --build-arg HTTPS_PROXY=${proxy} \
+        --build-arg FTP_PROXY=${proxy} \
+        --build-arg http_proxy=${proxy} \
+        --build-arg https_proxy=${proxy} \
+        --build-arg ftp_proxy=${proxy} \
+        --build-arg no_proxy=${_no_proxy} .
+
+
 docker run -d --hostname=${container_prestashop_name} \
            --link ${container_db_name} \
            --name=${container_prestashop_name} \
-           -e DB_SERVER=some-mysql=${container_db_name}  \
+           -v ${PWD}/www:/var/www/html \
            -e PS_INSTALL_AUTO=1 \
+           -e PS_ERASE_DB=1 \
            -p ${forwarded_port}:80 \
            -d prestashop/prestashop
 
+
+docker exec ${container_prestashop_name} chmod 777 -R /var/www/html/
+docker exec ${container_prestashop_name} service apache2 restart
 docker exec -it ${container_prestashop_name} /bin/bash
