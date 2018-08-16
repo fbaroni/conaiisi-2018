1fc9cae8e541fd176aebb6c5a8e713da1c649866
root <jonatanduttweiler@gmail.com>
2018-08-16 09:55:18 -0300

    un par de mejoras con respecto a anoche: 1. La base esta funcionando 2. El web server esta funcionando 3. entre el ws y la base hay conexion (se crearon las tablas en la bd prestashop)

--- docker-infraestructure/dockerlab_conaiisi/prestashop/restart.sh
+++ docker-infraestructure/dockerlab_conaiisi/prestashop/restart.sh
@@ -3,30 +3,45 @@
 #Es necesario que este contenedor se conecte a la misma docker network que el contenedor que contiene la base
 #mysql para poder usar la resolucion de nombres de docker
 inst=1
+image_name=jduttweiler/prestashop:1.7
 container_name=prestashop_${inst}
 mysql_container_name=pmysql
 container_db_net=db_network
 container_balancer_net=bal_network
+local_ip=10.4.9.66
 
-docker stop ${container_name} || true && docker rm ${container_name} || true
+docker stop ${container_name} && docker rm ${container_name}
 
-#check if network exists?
+#BUILD DOCKER IMAGE
+proxy=${http_proxy}
+_no_proxy=${no_proxy} #Should be localhost,127.0.0.1,::1,santafe.gob.ar,santafe.gov.ar
+
+docker build \
+        -t  ${image_name} \
+        --build-arg HTTP_PROXY=${proxy} \
+        --build-arg HTTPS_PROXY=${proxy} \
+        --build-arg FTP_PROXY=${proxy} \
+        --build-arg http_proxy=${proxy} \
+        --build-arg https_proxy=${proxy} \
+        --build-arg ftp_proxy=${proxy} \
+        --build-arg no_proxy=${_no_proxy} .
+
 #docker network create ${container_db_net}
 
+#DOCKER RUN CONTAINER
 docker run -ti \
 	   --name ${container_name} \
 	   --hostname ${container_name} \
 	   --network ${container_db_net} \
 	   -e DB_SERVER=${mysql_container_name} \
+           -e PS_DEV_MODE=1\
+           -e PS_INSTALL_AUTO=1\
+           -e PS_ERASE_DB=1\
+	   -e PS_DOMAIN=${local_ip}:8007\
            -e DB_USER=ps_user\
            -e DB_PASSWD=ps_password\
- 	   --publish-all\
-           -d prestashop/prestashop
-
-#           -e PS_DEV_MODE=1\
-#           -e PS_INSTALL_AUTO=1\
-#           -e PS_ERASE_DB=1\
-
+ 	   -p 8007:80\
+           -d jduttweiler/prestashop:1.7
 
 
 #Connect container to external network
