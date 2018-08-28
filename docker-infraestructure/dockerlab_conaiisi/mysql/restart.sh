603a6c47ad876f24eb7862c32d3346d722450502
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2018-08-28 20:54:52 -0300

    Agregado de volumes a los contenedores mysql y prestashop

--- docker-infraestructure/dockerlab_conaiisi/mysql/restart.sh
+++ docker-infraestructure/dockerlab_conaiisi/mysql/restart.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 #Este script levanta un contenedor conteniendo una base de datos mysql
 
-#Create directory for the database
-host_directory_db=/mysql/prestashop
-mkdir -p ${host_directory_db}
-#chmod
-chmod 777 ${host_directory_db}
-
-
 container_name=pmysql
 container_net=db_network
 mysql_v=5
 
-docker stop ${container_name} || true && docker rm ${container_name} || true
+docker stop ${container_name} && docker rm ${container_name} 
 
 #check if network exists?
 #docker network create ${container_net}
-
 docker run -ti \
-	   --name ${container_name} \
-           -h ${container_name}\
-           -v ${host_directory_db}:/var/lib/mysql \
-           -e MYSQL_DATABASE=prestashop\
- 	   -e MYSQL_ROOT_PASSWORD=password\
-  	   -e MYSQL_USER=ps_user\
-           -e MYSQL_PASSWORD=ps_password\
-	   -d mysql:${mysql_v}
+		--name ${container_name} \
+		--mount source=mysql-volume,target=/var/lib/mysql \
+		--hostname ${container_name}\
+		-e MYSQL_DATABASE=prestashop\
+		-e MYSQL_ROOT_PASSWORD=password\
+		-e MYSQL_USER=ps_user\
+		-e MYSQL_PASSWORD=ps_password\
+		-d mysql:${mysql_v}
 
 #Connect container to internal network
 docker network connect ${container_net} ${container_name}
--- docker-infraestructure/dockerlab_conaiisi/prestashop/restart.sh
+++ docker-infraestructure/dockerlab_conaiisi/prestashop/restart.sh
@@ -2,7 +2,7 @@
 #Este script levanta un contenedor con prestashop
 #Es necesario que este contenedor se conecte a la misma docker network que el contenedor que contiene la base
 #mysql para poder usar la resolucion de nombres de docker
-inst=2
+inst=1
 image_name=jduttweiler/prestashop:1.7
 container_name=prestashop_${inst}
 mysql_container_name=pmysql
@@ -12,48 +12,74 @@ host_name=localhost
 host_external_port=8008
 container_external_port=80
 
+#Volume
+#Create directory for the database
+#docker volume create prestashop-volume
+
 docker stop ${container_name} && docker rm ${container_name}
 
 #BUILD DOCKER IMAGE
 proxy=${http_proxy}
 _no_proxy=${no_proxy} #Should be localhost,127.0.0.1,::1,santafe.gob.ar,santafe.gov.ar
 
-docker build \
-        -t  ${image_name} \
-        --build-arg HTTP_PROXY=${proxy} \
-        --build-arg HTTPS_PROXY=${proxy} \
-        --build-arg FTP_PROXY=${proxy} \
-        --build-arg http_proxy=${proxy} \
-        --build-arg https_proxy=${proxy} \
-        --build-arg ftp_proxy=${proxy} \
-        --build-arg no_proxy=${_no_proxy} .
-
+if [ -n "$proxy" ] ; then
+	echo "Build using proxy:"${proxy}
+	docker build \
+		-t  ${image_name} \
+		--build-arg HTTP_PROXY=${proxy} \
+		--build-arg HTTPS_PROXY=${proxy} \
+		--build-arg FTP_PROXY=${proxy} \
+		--build-arg http_proxy=${proxy} \
+		--build-arg https_proxy=${proxy} \
+		--build-arg ftp_proxy=${proxy} \
+		--build-arg no_proxy=${_no_proxy} .
+else
+	echo "Build without using proxy"
+	docker build -t ${image_name} .
+fi
 #docker network create ${container_db_net}
 
 #DOCKER RUN CONTAINER
-docker run -ti \
-	   --name ${container_name} \
-	   --hostname ${container_name} \
-	   --network ${container_db_net} \
-	   -e DB_SERVER=${mysql_container_name} \
-           -e DB_USER=ps_user\
-           -e DB_NAME=prestashop\
-           -e DB_PASSWD=ps_password\
-           -e HTTP_PROXY=${proxy} \
-           -e HTTPS_PROXY=${proxy} \
-           -e FTP_PROXY=${proxy} \
-           -e http_proxy=${proxy} \
-           -e https_proxy=${proxy} \
-           -e ftp_proxy=${proxy} \
- 	   -p ${host_external_port}:${container_external_port}\
-           -d jduttweiler/prestashop:1.7
 
-#          -e PS_DEV_MODE=1\
-#          -e PS_INSTALL_AUTO=1\
-#          -e PS_ERASE_DB=1\
-#           -e PS_DOMAIN=${container_name}:${container_external_port}\
+if [ -n "$proxy" ] ; then
+	echo "Run container with proxy"${proxy}
+	docker run -ti \
+		--name ${container_name} \
+		--hostname ${container_name} \
+		--network ${container_db_net} \
+		--mount source=prestashop-volume,target=/var/www/html \
+		-e DB_SERVER=${mysql_container_name} \
+		-e DB_USER=ps_user\
+		-e DB_NAME=prestashop\
+		-e DB_PASSWD=ps_password\
+		-e HTTP_PROXY=${proxy} \
+		-e HTTPS_PROXY=${proxy} \
+		-e FTP_PROXY=${proxy} \
+		-e http_proxy=${proxy} \
+		-e https_proxy=${proxy} \
+		-e ftp_proxy=${proxy} \
+		-p ${host_external_port}:${container_external_port}\
+		-d jduttweiler/prestashop:1.7
+else
+	echo "Run container without proxy"
+	docker run -ti \
+		--name ${container_name} \
+		--hostname ${container_name} \
+		--network ${container_db_net} \
+		--mount source=prestashop-volume,target=/var/www/html \
+		-e DB_SERVER=${mysql_container_name} \
+		-e DB_USER=ps_user\
+		-e DB_NAME=prestashop\
+		-e DB_PASSWD=ps_password\
+		-e PS_DOMAIN=localhost:${host_external_port}\
+		-p ${host_external_port}:${container_external_port}\
+		-d jduttweiler/prestashop:1.7
+fi
 
-
+#Erase and initialize the database
+#		-e PS_DEV_MODE=1\
+#		-e PS_INSTALL_AUTO=1\
+#		-e PS_ERASE_DB=1\
 
 #Connect container to external network
 #docker network connect ${container_balancer_net} ${container_name}
